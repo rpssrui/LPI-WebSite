e03dcb498c8e235c3e44333634c1f1d9b8413754
Rui Silva <38170@ufp.edu.pt>
2022-03-13 12:01:43 +0000

    website-LPI

---
+++ env/app.py
@@ -0,0 +1,37 @@
+from xml.sax import make_parser
+from flask import Flask, redirect, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import folium
+
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db=SQLAlchemy(app)
+   
+#funcoes aux
+def getLongitude():
+    longitude=-8.60680757276
+    return longitude
+
+def getLatitude():
+    latitude=41.1706859839
+    return latitude
+
+@app.route('/',methods=['POST','GET'])
+def index():
+        return render_template('index.html')
+
+@app.route('/mapa',methods=['POST','GET'])
+def localization():
+    start_coords = (41.1579438,-8.6291053)
+    mapa = folium.Map(location=start_coords, zoom_start=9)
+    folium.Marker(
+        location=[getLatitude(),getLongitude()],
+        popup="Universidade Fernando Pessoa",
+        icon=folium.Icon(color="red", icon="info-sign"),
+        ).add_to(mapa)
+    return mapa._repr_html_()
+        
+if __name__=="__main__":
+    app.run(debug=True)
